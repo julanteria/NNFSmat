e5d492adfa3b61bd51d083ec20b33bdefa6587ec
Julian Vorpahl <julian.vorpahl@raylytic.com>
2022-06-11 13:27:36 +0200

    dot product

--- first_layer.m
+++ first_layer.m
@@ -7,7 +7,7 @@ weights=[0.2, 0.8, -0.5, 1;
 
 biases = [2, 3, 0.5];
 
-layer_output = cell(0,3);
+layer_output = zeros(1,3);
 
 
 
@@ -23,9 +23,27 @@ for i=1:length(biases)
     
     neuron_out = neuron_out + biases(i);
     
-    layer_out(i) = neuron_out;
+    layer_output(i) = neuron_out;
     %disp(biases(i));
     %disp(weights(i,:));
 end 
-disp(layer_out);
+disp(layer_output);
+
+
+layer_output2 = inputs * weights' + biases;
+
+disp(layer_output2);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
