e2011a95b578f664b1cb17fd7f550e1d751b0af8
Julian Vorpahl <julian.vorpahl@raylytic.com>
2022-06-15 12:31:14 +0200

    Kind of implemented n spiral data

--- chapters/chp3/spiral_data.m
+++ chapters/chp3/spiral_data.m
@@ -1,48 +1,124 @@
-%{
 
 
 
-% given values
-pos = [8 4 ;    % startpoint
-       2 7 ] ;  % endpoint
-nturns = 3 ;    % number of turns (integer value)
-% engine
-dp = diff(pos,1,1) ;
-R = hypot(dp(1), dp(2)) ;
-phi0 = atan2(dp(2), dp(1)) ;
-phi = linspace(0, nturns*2*pi, 10000) ; % 10000 = resolution
-r = linspace(0, R, numel(phi)) ;
-x = pos(1,1) + r .* cos(phi + phi0) ;
-y = pos(1,2) + r  .* sin(phi + phi0) ;
-%plot(x,y,'b-',pos(:,1),pos(:,2),'ro-') ; % nturns crossings, including end point
-%}
 
 
-%to do
-%make random value from normal dist work: normrnd in matlab,
-%np.random.randn() in python example
+data = nspirals(4);
 
-samples = 100;
-classes = 3;
-
-X = zeros(samples*classes,2);
-
-y = zeros(samples*classes,1);
+scatter(data(:,1), data(:,2), 12, data(:,3)); axis equal;
 
 
 
-for class_number=0:classes-1
+
+function data = nspirals(n , N, degrees, start)
+% Generate "n spirals" dataset with N instances.
+% degrees controls the length of the spirals
+% start determines how far from the origin the spirals start, in degrees
+% noise displaces the instances from the spiral. 
+%  0 is no noise, at 1 the spirals will start overlapping
+
+    if nargin < 1
+        n = 2;
+    end
+    if nargin < 2
+        N = 4000;
+    end
+    if nargin < 3
+        degrees = 300;
+    end
+    if nargin < 4
+        start = 50;
+    end
+
     
-    ix = [samples*class_number:1:samples*(class_number+1)];
+    noise = 0;
+    deg2rad = (2*pi)/360;
+    start = start * deg2rad;
+    N1 = floor(N/2);
+    N2 = N-N1;
+    data = zeros(floor(N/2),3);
+ 
     
-    r = linspace(0, 1, samples) ;
+    for i=1:n
+    
+        if mod(i,2) == 0
+            N = N1;
+            n = start + sqrt(rand(N,1)) * degrees * deg2rad;
+            d = [-cos(n)/i.*n + rand(N,1)*noise sin(n)/i.*n+rand(N,1)*noise zeros(N,1)+i];
+            
+        
+        else
+            N = N2;
+            n = start + sqrt(rand(N,1)) * degrees * deg2rad;
+            d = [cos(n)/i.*n+rand(N,1)*noise -sin(n)/i.*n+rand(N,1)*noise zeros(N,1)+i];
+            
+        end
+        
+        
+        dat = data;
+        data = [dat;d];
+        
+           
+        
+    end
     
     
-    t = linspace() ;
-    %t = np.linspace(class_number*4, (class_number+1)*4, samples) + np.random.randn(samples)*0.2
-    
-    disp(ix)
 end
+
+
+
+function data = twospirals(N, degrees, start, noise)
+% Generate "two spirals" dataset with N instances.
+% degrees controls the length of the spirals
+% start determines how far from the origin the spirals start, in degrees
+% noise displaces the instances from the spiral. 
+%  0 is no noise, at 1 the spirals will start overlapping
+    if nargin < 1
+        N = 2000;
+    end
+    if nargin < 2
+        degrees = 570;
+    end
+    if nargin < 3
+        start = 90;
+    end
+    if nargin < 5
+        noise = 0.2;
+    end  
+    
+    deg2rad = (2*pi)/360;
+    start = start * deg2rad;
+    N1 = floor(N/2);
+    N2 = N-N1;
+    
+    n = start + sqrt(rand(N1,1)) * degrees * deg2rad;   
+    d1 = [-cos(n).*n + rand(N1,1)*noise sin(n).*n+rand(N1,1)*noise zeros(N1,1)];
+    
+    n = start + sqrt(rand(N1,1)) * degrees * deg2rad;      
+    d2 = [cos(n).*n+rand(N2,1)*noise -sin(n).*n+rand(N2,1)*noise ones(N2,1)];
+    
+    data = [d1;d2];
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
